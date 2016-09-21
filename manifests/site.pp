d488a33cfaa860bd7275c0598bb1693534e1e8e7
radiospares <clint.day08@gmail.com>
2016-09-21 12:39:26 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -52,7 +52,7 @@ node default {
     #  }
       
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
-      path => '/etc/motd',
+      path => '/usr/local/bin',
       creates => '/etc/motd',
       }
 }
