3cc590bdcab87ee6e1a5a3eb9b3bdacd954893d9
root <root@radiospares.puppetlabs.vm>
2016-09-22 18:35:27 +0000

    modified:   init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -6,7 +6,7 @@ class memcached {
       ensure => file,
       owner => root,
       group => root,
-      mode => 644,
+      mode => '0644',
       source => 'puppet:///modules/memcached/memcached',
       require => Package['memcached'],
     }
