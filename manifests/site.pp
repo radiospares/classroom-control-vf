3991e2baae5e71b208ab45f36a9895d5a60d9412
root <root@radiospares.puppetlabs.vm>
2016-09-23 17:08:56 +0000

    modified:   site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -60,4 +60,12 @@ node default {
 #  include skeleton
   include memcached
   include nginx
+
+  node default {
+    if $::virtual != 'physical' {
+      $vmname = capitalize($::virtual)
+      notify { "This is a ${vmname} virtual machine.": }
+      }
+    }
+
 }
