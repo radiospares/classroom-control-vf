c31e429aa42ea037c6129142abd10ae25960252f
root <root@radiospares.puppetlabs.vm>
2016-09-23 21:58:59 +0000

    modified:   site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,9 +42,8 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-
-  $message = hiera('message')
-    notify { $message: }
+$message = hiera('message')
+notify { $message: }
 
     }
   
